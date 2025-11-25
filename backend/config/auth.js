53a77b8e3f330f408d6c88fe4347179f3ff9557b
czafa <caiozafa25@gmail.com>
2025-11-24 21:42:06 -0300

    feat: add `auth.js`

---
+++ backend/config/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = "1h";
+
+if (!JWT_SECRET) {
+  throw new Error("chave de acesso não configurada no .env");
+}
+
+function gerarToken(payload) {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+}
+
+function verificarToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
+
+module.exports = {
+  gerarToken,
+  verificarToken,
+};
